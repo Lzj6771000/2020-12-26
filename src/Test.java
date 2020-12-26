245eebee0a917429a7bd1026373c581224ea599a
zzh <584435748@qq.com>
2020-12-26 12:13:09 +0800

    上传第四次

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Test {
+    File file=new File("D:\2020-12-26\src\Users.xlsx");
+    ReadExcel MelonZhou;
+    Melonzhou. readExcel(file);
+
+}
---
+++ src/User.java
@@ -0,0 +1,38 @@
+public class User {
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String users) {
+        Username = users;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String adress) {
+        Address = adress;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    private String Username;
+    private String Password;
+    private String Address;
+    private String Phone;
+}
